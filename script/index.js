0e3d2fcc67f53920c1c62a4fac6549c1a228464b
badsha103868 <badshagolder5@gmail.com>
2025-09-09 19:40:32 +0600

    final english janala

--- script/index.js
+++ script/index.js
@@ -5,6 +5,13 @@ const createElements = (arr) =>{
   return htmlElements.join(" ");
 }
 
+// voice function
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = "en-EN"; // English
+  window.speechSynthesis.speak(utterance);
+}
+
 // spinner function
  const manageSpinner =  (status) =>{
   if(status==true){
@@ -126,7 +133,7 @@ const displayLevelWord = (words) =>{
       <div class="text-2xl font-medium font-bangla">${word.meaning?word.meaning:"অর্থ পাওয়া যায় নি"} / ${word.pronunciation ?word.pronunciation: "pronounciation পাওয়া যায় নি"}</div>
       <div class=" flex justify-between items-center ">
         <button onclick = "loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF90]"><i class="fa-solid fa-circle-info"></i></button>
-        <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF90]"><i class="fa-solid fa-volume-high"></i></button>
+        <button onclick= "pronounceWord('${word.word}')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF90]"><i class="fa-solid fa-volume-high"></i></button>
 
       </div>
     </div>
@@ -157,3 +164,24 @@ const displayLesson = (lessons)=>{
   
 }
 loadLesson();
+
+// search button event listener
+document.getElementById("btn-search").addEventListener("click",() =>{
+  // jakhon button a click korbo then active button hidden kore debe tar jonno removeActive k call kore diota hobe
+  removeActive();
+
+  const input =document.getElementById("input-search")
+  const searchValue = input.value.trim().toLowerCase();
+  // console.log(searchValue)
+  fetch("https://openapi.programming-hero.com/api/words/all")
+  .then((res) => res.json())
+  .then((data) =>{
+    const allWords =data.data;
+    console.log(allWords);
+    const filterWords = allWords.filter((word) => word.word.toLowerCase().includes(searchValue));
+    // console.log(filterWords);
+    // display korar jonno
+    displayLevelWord(filterWords);
+  })
+  
+})
